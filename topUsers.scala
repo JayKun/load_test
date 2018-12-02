c7ccbe68277886b126e5e77965a8f5ba425cf5ac
JayKun <junkaiong11@gmail.com>
2018-12-02 22:57:51 +0000

    Add proper naming

--- topUsers.scala
+++ topUsers.scala
@@ -2,13 +2,13 @@ val fileRdd = sc.textFile("twitter.edges")
 
 val splitRdd = fileRdd.map( line => line.split(": "))
 
-val yourRdd = splitRdd.flatMap( arr => {
+val mapRdd = splitRdd.flatMap( arr => {
     val user = arr(0)
     val users = arr(1).split(",")
     users.map( followed => (followed, 1))
 })
 
-val reducedRdd = yourRdd.reduceByKey((a,b) => a+b)
+val reducedRdd = mapRdd.reduceByKey((a,b) => a+b)
 
 val res = reducedRdd.filter((e) => (e._2 > 1000))
 
